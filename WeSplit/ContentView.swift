aa74f8058b9456040e06e11395bc14ea75f6e635
Ian Waddington <iwaddo@icloud.com>
2022-11-13 15:21:17 +0000

    After TapCount

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,15 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tapCount = 0
+    
     var body: some View {
-        NavigationView {
-            Form {
-                Section {
-                    Text("Hello, New World!")
-                }
-            }
-            .navigationTitle("SwiftUI")
-            .navigationBarTitleDisplayMode(.inline)
+        Button("Tap Count: \(tapCount)") {
+            tapCount += 1
         }
     }
 }
