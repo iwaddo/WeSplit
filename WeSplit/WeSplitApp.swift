98aafbac8db5b8b54391844ab084fef60d0dbfb4
Ian Waddington <iwaddo@icloud.com>
2022-11-13 14:18:37 +0000

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Ian Waddington on 13/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
